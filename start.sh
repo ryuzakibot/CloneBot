5a433af0278fadfcd5ba666b1eb2512c5c0b6534
ryuzakibot <97783267+ryuzakibot@users.noreply.github.com>
2022-03-02 15:17:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ telegram_token = $telegram_token
 user_ids = $user_ids
 group_ids = $group_ids
 gclone_para_override = $gclone_para_override" >> "telegram_gcloner/config.ini"
+pip install -r requirements.txt
 npm install http-server -g
 http-server -p $PORT &
 python3 telegram_gcloner/telegram_gcloner.py
